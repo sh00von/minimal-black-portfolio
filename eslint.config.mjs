a798ab743a806d18cb648dc8d94e0ebbeab9e682
sh00von <u2110015@student.cuet.ac.bd>
2025-05-23 19:43:28 +0600

    Remove deprecated ESLint configuration file and integrate custom rule overrides directly into the ESLint config for TypeScript and React.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
